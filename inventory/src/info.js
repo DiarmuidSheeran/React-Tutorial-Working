1cdbd1bef7889db702daa8c4f6f4c56f4292952a
Diarmuid Sheeran <dssheeran@outlook.com>
2024-07-24 20:44:45 +0000

    Convert function component to class based component

--- inventory/src/info.js
+++ inventory/src/info.js
@@ -1,16 +1,24 @@
-export default function Info() {
-    const title = "Inventory System"
-    const showTitle = true;
-  
-    if (showTitle) {
-      return (
-        <div>
-          <h1>{title}</h1>
-          <p>Manage your stuff.</p>
-        </div>
-      );
-    } else {
-      return <p>Empty</p>
+import React from "react";
+
+class Info extends React.Component {
+    render() {
+        const title = "Inventory System"
+        const showTitle = true;
+
+        if (showTitle) {
+            return (
+            <div>
+                <h1>{title}</h1>
+                <p>Manage your stuff.</p>
+            </div>
+            );
+        } else {
+            return <p>Empty</p>
+        }
     }
-  }
-    
+}
+
+
+
+export default Info;
+
