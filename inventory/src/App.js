30835b93c68edc2a81f75678adb3b78342df360d
Diarmuid Sheeran <dssheeran@outlook.com>
2024-07-24 20:34:02 +0000

    Edit app.js filew to add a default value to input field

--- inventory/src/App.js
+++ inventory/src/App.js
@@ -16,20 +16,26 @@ function Info() {
   const title = "Inventory System"
   const showTitle = true;
 
-  return (
-    <div>
-      <h1>{showTitle ? title : ""}</h1>
-      <p>Manage your stuff.</p>
-    </div>
-  );
+  if (showTitle) {
+    return (
+      <div>
+        <h1>{title}</h1>
+        <p>Manage your stuff.</p>
+      </div>
+    );
+  } else {
+    return <p>Empty</p>
+  }
 }
+  
 
 function AddItem() {
+  const value = "default";
   return (
     <div>
       <form>
         <label for="text-form">Type Something</label>
-        <input type="text" id="text-form"/>
+        <input type="text" value={value} id="text-form"/>
         <Info />
       </form>
     </div>
